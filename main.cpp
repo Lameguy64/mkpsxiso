9b1befebb903e96f8137a67a416f0d05507da179
Devon Meunier <devon.meunier@shopify.com>
2021-01-12 09:42:52 -0500

    Version v0.32

--- main.cpp
+++ main.cpp
@@ -491,7 +491,7 @@ void ParseISO(cd::IsoReader& reader) {
 int main(int argc, char *argv[]) {
 
 
-    printf("isodump v0.31 - PlayStation ISO dumping tool\n");
+    printf("isodump v0.32 - PlayStation ISO dumping tool\n");
     printf("2017 Meido-Tek Productions (Lameguy64), 2020 Phoenix (SadNES cITy).\n\n");
 
 	if (argc == 1) {
