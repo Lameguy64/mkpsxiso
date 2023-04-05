e36a207a7e20c8f5ccfd58cf0906aae9d2192e58
retro <43223593+retro-git@users.noreply.github.com>
2023-04-05 20:09:21 +0100

    fix stat64 build errors on apple silicon (#33)
    Co-authored-by: retro <retro@retros-mbp.home>
    Co-authored-by: retro <retro@retros-mbp-2.home>

--- src/shared/platform.h
+++ src/shared/platform.h
@@ -13,6 +13,12 @@
 #ifdef _WIN32
 #define stat64 _stat64
 #define PRFILESYSTEM_PATH "ls"
+#elif defined(__APPLE__) && defined(__arm64__)
+// __DARWIN_ONLY_64_BIT_INO_T is set on ARM-based Macs (which then sets __DARWIN_64_BIT_INO_T).
+// This sets the following in Apple SDK's stat.h: struct stat __DARWIN_STRUCT_STAT64;
+// So use stat over stat64 for ARM-based Macs
+#define stat64 stat
+#define PRFILESYSTEM_PATH "s"
 #else
 #define PRFILESYSTEM_PATH "s"
 #endif
